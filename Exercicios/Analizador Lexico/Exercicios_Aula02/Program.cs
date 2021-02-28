77f35e08e9d17a05410fc13bd54c2bf7bb819d00
Guilherme Rocha <guirochas17@gmail.com>
2021-02-27 22:12:37 -0300

    Adição dos exercicios

---
+++ Exercicios/Analizador Lexico/Exercicios_Aula02/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Exercicios_Aula02
+{
+    class Program
+    {
+        public static void Exercicio_01()
+        {
+            int quantidadeLetras = 0;
+            int quantidadeNumeros = 0;
+            int quantidadeOutrosChars = 0;
+
+            using (FileStream fs = new FileStream(@$"{ Directory.GetCurrentDirectory() }/file_tmp.txt", FileMode.Open, FileAccess.Read))
+            {
+                char currentByte;
+
+                for(int i = 0; i < (int) fs.Length; i++) 
+                {
+                    currentByte = (char) fs.ReadByte();
+
+                    if (char.IsLetter(currentByte))
+                        quantidadeLetras++;
+                    else
+                    {
+                        if (char.IsNumber(currentByte))
+                            quantidadeNumeros++;
+                        else
+                            quantidadeOutrosChars++;
+                    }
+                }
+
+                Console.WriteLine(" =========== Exercicio 01 ===========");
+                Console.WriteLine($"Quantidade de Numeros: {quantidadeNumeros}");
+                Console.WriteLine($"Quantidade de Letras: {quantidadeLetras}");
+                Console.WriteLine($"Quantidade de Outros Chars: {quantidadeOutrosChars}");
+            }
+        }
+
+        public static void Exercicio_02()
+        {
+            if (!File.Exists(@$"{Directory.GetCurrentDirectory()}/file_tmp.txt"))
+                File.Create(@$"{Directory.GetCurrentDirectory()}/file_tmp.txt");
+            else
+                File.WriteAllText(@$"{Directory.GetCurrentDirectory()}/file_tmp.txt", string.Empty);
+
+            using (StreamReader sr = new StreamReader("Teste.txt"))
+            {
+                Console.WriteLine(" =========== Exercicio 02 ===========");
+
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    line = Regex.Replace(line, @"#(.*?)#", string.Empty, RegexOptions.IgnoreCase).ToUpper();
+                    using (StreamWriter sw = new StreamWriter(@$"{Directory.GetCurrentDirectory()}/file_tmp.txt", true))
+                    {
+                        Console.WriteLine(line);
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Exercicio_01();
+            Console.WriteLine("\n\n");
+            Exercicio_02();
+        }
+    }
+}
---
+++ Exercicios/Analizador Lexico/Exercicios_Aula03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Exercicios_Aula03
+{
+    class Program
+    {
+        public static void AnaliseLexicaTexto()
+        {
+            int numeroLinha = 1;
+            StreamReader leitorEmStream = new StreamReader("./Teste.txt");
+            int tamanhoArquivo = (int) leitorEmStream.BaseStream.Length;
+
+            for (int i = 0; i < tamanhoArquivo; i++)
+            {
+                char byteArquivo = (char) leitorEmStream.BaseStream.ReadByte();
+
+                if (char.IsDigit(byteArquivo))
+                {
+                    string valor = "";
+                    do
+                    {
+                        valor += byteArquivo.ToString();
+                        i++;
+                        byteArquivo = (char)leitorEmStream.BaseStream.ReadByte();
+                    }
+                    while (char.IsDigit(byteArquivo));
+
+                    Console.WriteLine($"Linha {numeroLinha} => {valor}");
+                }
+
+                if (byteArquivo == 13)
+                {
+                    numeroLinha++;
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            AnaliseLexicaTexto();
+            Console.ReadKey();
+        }
+    }
+}
